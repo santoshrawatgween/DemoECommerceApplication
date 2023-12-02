8c9416d0542d494aeb8698627e9bee55924d232a
santo <santo@192.168.1.36>
2023-12-02 11:35:41 +0530

    zscs

--- src/main/java/genricLib/BaseClass.java
+++ src/main/java/genricLib/BaseClass.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -22,7 +25,23 @@ public class BaseClass
 	public void openApp() throws FileNotFoundException, IOException
 	{
 		WebDriverManager.chromedriver().setup();
-		driver= new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		WebDriverManager.edgedriver().setup();
+		String browserName= pdata.getData("browserName");
+		if(browserName.contains("chrome"))
+		{
+			driver= new ChromeDriver();
+		}
+		
+		if(browserName.contains("firefox"))
+		{
+			driver= new FirefoxDriver();
+		}
+		if(browserName.contains("edge"))
+		{
+			driver= new EdgeDriver();
+		}
+		
 		driver.manage().window().maximize();
 		driver.get(pdata.getData("url"));
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -33,7 +52,6 @@ public class BaseClass
 	public void closeApp(ITestResult its) throws InterruptedException, IOException
 	{
 		Thread.sleep(3000);
-		
 		int status = its.getStatus();
 		String name = its.getName();
 		if(status==2)
